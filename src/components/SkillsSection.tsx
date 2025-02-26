b2cbc75817644bac88c3ee092893f9c4abf2ae9f
AlannaParsons <alannagparsons@gmail.com>
2025-02-26 10:28:24 -0700

    make tooltips useable for mobile/touch event

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,14 +1,15 @@
+"use client"
+import { useState } from 'react';
 import {Image, Space, Tooltip} from '@mantine/core';
 import classes from './SkillsSection.module.css';
-
 
 export default function SkillsSection() {
   //maintain consistent naming with icon img files
   const skills = ['React', 'C', 'Python', 'Javascript', 'Html5', 
     'Css3', 'Github', 'Jest', 'Mocha', 'NextJS', 'PostgreSQL', 'Tailwind',
     'Vercel', 'Vim', 'VScode'
-  ]
-
+  ];
+  
 return(
   <div className="flex flex-1 flex-col mt-10"> 
 
@@ -21,15 +22,19 @@ return(
     <div className="grid xl:grid-cols-4  lg:grid-cols-5  grid-cols-4 gap-4 bg-inherit p-5">
 
       {skills.map((skillName:string)=>{
-        return <Tooltip label={skillName} key={skillName} >
-          <div className="bg-rose rounded-xl" >  
-            <Image       
-              src={`/icons/tech/${skillName.toLocaleLowerCase()}.png`}
-              alt={skillName} 
-              className={classes.skillIcons}
-            />
-          </div>
-        </Tooltip>
+        return <Tooltip 
+          label={skillName} 
+          key={skillName} 
+          events={{ hover: true, focus: true, touch: true }}
+          >
+            <div className="bg-rose rounded-xl" >  
+              <Image       
+                src={`/icons/tech/${skillName.toLocaleLowerCase()}.png`}
+                alt={skillName} 
+                className={classes.skillIcons}
+              />
+            </div>
+          </Tooltip>
 
     })}
     </div>
